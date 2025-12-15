21a7097bb763cb70559f010d667d741dd0fb8d98
Joe Riley <joe.riley@enable.com>
2025-12-15 15:04:26 +0000

    Update packages

--- samples/graph-teams-tag/csharp/GraphTeamsTag/ClientApp/src/index.js
+++ samples/graph-teams-tag/csharp/GraphTeamsTag/ClientApp/src/index.js
@@ -1,14 +1,15 @@
 import { Provider, teamsTheme } from '@fluentui/react-northstar';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { AppRoute } from './router/router';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider theme={teamsTheme}>
     <React.StrictMode>
       <AppRoute />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
