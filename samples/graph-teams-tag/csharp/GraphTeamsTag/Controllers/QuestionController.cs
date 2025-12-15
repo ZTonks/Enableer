d0b2ac83f773aaeb9c48d8a1456ec26d63385758
Zak Tonks <zak.tonks@enable.com>
2025-12-15 15:41:47 +0000

    Yes

---
+++ samples/graph-teams-tag/csharp/GraphTeamsTag/Controllers/QuestionController.cs
@@ -0,0 +1,90 @@
+﻿using GraphTeamsTag.Helper;
+using GraphTeamsTag.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+
+namespace GraphTeamsTag.Controllers;
+
+[Route("api/questions")]
+[ApiController]
+public class QuestionController : Controller
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<QuestionController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public QuestionController(
+        ILogger<QuestionController> logger,
+        IConfiguration configuration,
+        IHttpClientFactory httpClientFactory,
+        IHttpContextAccessor httpContextAccessor,
+        GraphHelper graphHelper)
+    {
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> AskQuestion(
+        [FromQuery] string ssoToken,
+        AskQuestionRequest request)
+    {
+        var token = await SSOAuthHelper.GetAccessTokenOnBehalfUserAsync(_configuration, _httpClientFactory, _httpContextAccessor, ssoToken);
+        var graphClient = SimpleGraphClient.GetGraphClient(token);
+
+        var members = (await graphClient.Teams[request.TeamId].Tags[request.Tag].Members.Request().GetAsync()).ToList();
+
+        if (request.TargetsOnlineUsers)
+        {
+            var onlineMembers = new List<TeamworkTagMember>();
+
+            foreach (var member in members)
+            {
+                var userPresence = await graphClient.Users[member.UserId].Presence.Request().GetAsync();
+
+                if (userPresence.Availability != "Available")
+                {
+                    onlineMembers.Add(member);
+                }
+            }
+
+            members = onlineMembers;
+        }
+
+        if (request.Email)
+        {
+            // ZT TODO
+            return Ok();
+        }
+
+        var chat = new Chat
+        {
+            ChatType = request.QuestionTarget == QuestionTarget.All ? ChatType.Group : ChatType.OneOnOne,
+        };
+
+        foreach (var member in members)
+        {
+            chat.Members.Add(new AadUserConversationMember
+            {
+                Roles = new List<string>()
+                {
+                    "owner"
+                },
+                AdditionalData = new Dictionary<string, object>()
+                {
+                    {"user@odata.bind", $"https://graph.microsoft.com/v1.0/users('{member.UserId}')"}
+                }
+            }
+            );
+        }
+
+        chat.Messages
+
+        var response = await graphClient.Chats.Request().AddAsync(chat);
+
+        return Ok();
+    }
+}
